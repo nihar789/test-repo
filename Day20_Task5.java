92105b3c49d0e83e6ec5a53d8ade00fbb0a90bcc
nihar786 <niharashraf97@gmail.com>
2025-07-26 16:16:24 +0530

    lower Bounded Wildcards

--- Day20_Task5.java
+++ Day20_Task5.java
@@ -1,29 +1,30 @@
-interface Shape {
-    double area();
-}
-
-class Square implements Shape {
-    int height;
-    Square(int height) {
-        this.height = height;
-    }
-    public double area() {
-        return height * height;
-    }
-}
-
-class Circle implements Shape {
-    int r;
-    Circle(int r) {
-        this.r = r;
-    }
-    public double area() {
-        return Math.PI * r * r;
-    }
-}
-
-class ShapeComparator {
-    public double compareArea(Shape a, Shape b) {
-        return a.area() - b.area();
-    }
-}
+//interface Shape {
+//    double area();
+//}
+//
+//class Square implements Shape {
+//    int height;
+//    Square(int height) {
+//        this.height = height;
+//    }
+//    public double area() {
+//        return height * height;
+//    }
+//}
+//
+//class Circle implements Shape {
+//    int r;
+//    Circle(int r) {
+//        this.r = r;
+//    }
+//    public double area() {
+//        return Math.PI * r * r;
+//    }
+//}
+//
+//class ShapeComparator {
+//    public double compareArea(Shape a, Shape b) {
+//        return a.area() - b.area();
+//    }
+//}
+//
